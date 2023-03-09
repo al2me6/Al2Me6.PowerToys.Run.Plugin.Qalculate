40a36df35e846c66493cf0758a3d142d72bbfb8e
Alvin Meng <alvin.q.meng@gmail.com>
2023-03-08 22:14:18 -0500

    fix: reword plugin description Closes #1.

--- Al2Me6.PowerToys.Run.Plugin.Qalculate/Properties/Resources.Designer.cs
+++ Al2Me6.PowerToys.Run.Plugin.Qalculate/Properties/Resources.Designer.cs
@@ -88,7 +88,7 @@ namespace Al2Me6.PowerToys.Run.Plugin.Qalculate.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Advanced unit-aware calculator. `qalc.exe` must be available from the PATH. e.g., &quot;7g/cm^3 * 1 in^3&quot;, &quot;integrate(x * sqrt(x))&quot;, &quot;75 oF to oC&quot;, &quot;0o1234 to hex&quot;..
+        ///   Looks up a localized string similar to Advanced unit-aware calculator (e.g., &quot;7g/cm^3 * 1 in^3&quot;, &quot;integrate(x * sqrt(x))&quot;, &quot;75 oF to oC&quot;, &quot;0o1234 to hex&quot;). `qalc.exe` must be available from the `PATH`!.
         /// </summary>
         public static string plugin_description {
             get {
